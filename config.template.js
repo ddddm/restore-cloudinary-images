38488415e1f04e4163082cf8d0450b9d10ab2917
Dmitry Melnikov <m1290d@gmail.com>
2019-03-19 11:04:04 +0500

    add config template

---
+++ config.template.js
@@ -0,0 +1,7 @@
+module.exports = {
+    cloud_name: 'string', 
+    api_key: 'string', 
+    api_secret: 'string',
+    excel_file_in_the_root_folder: 'string:filename',
+    start_from_index_in_list: 0,
+}
